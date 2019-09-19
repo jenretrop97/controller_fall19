19bcd4b3c819aa9467650faff1b403640f4a1271
Jennifer Porter <jlporter@myhiddencove.com>
2019-09-18 18:54:06 -0600

    counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { value: 0 }
+
+  inc = () => {
+    this.setState( (state) => {
+      return { value: state.value + 1}
+    })
+    console.log(this.state.value)
+  }
+
+  dec() {
+    this.setState({ value: this.state.value - 1 })
+    this.setState( (state) => {
+      return { value: state.value - 1}
+    })
+    console.log(this.state.value)
+  }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className='colored'>Counter Example</h1>
+        <p>{this.state.value}</p>
+        <button onClick={this.inc}>+</button>
+        <button onClick={this.dec}>-</button>
+      </div>
+    )
+  }
 }
 
 export default App;
